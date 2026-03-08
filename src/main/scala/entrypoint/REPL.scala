849f2f13333bfefcac3641f0d2f0e67c066d2866
tolstovrob <tolstovrob@gmail.com>
2026-03-08 19:34:46 +0400

    feat: add newline support to REPL

--- src/main/scala/entrypoint/REPL.scala
+++ src/main/scala/entrypoint/REPL.scala
@@ -11,17 +11,35 @@ import utils.Printer.valueToString
 def repl(): Unit =
   val env = Builtins.builtins
   val globalEnv = Env(env, None)
+  
   println("[SCALISP]: running REPL")
+  println("Type :exit to quit")
+  
+  def processLine(line: String): Unit =
+    try
+      val expr = Parser.parse(line)
+      val value = eval(expr, globalEnv)
+      println(valueToString(value))
+    catch case e: Exception => println(s"Error: ${e.getMessage}")
+
+  def readMultiline(acc: String, openParens: Int): String =
+    if openParens == 0 && acc.nonEmpty then
+      acc
+    else
+      val prompt = if openParens > 0 then "... " else "> "
+      print(prompt)
+      val line = readLine()
+      
+      if line == null || line == ":exit" then
+        sys.exit(0)
+      else
+        val newAcc = if acc.isEmpty then line else s"$acc\n$line"
+        val newOpen = line.count(_ == '(') - line.count(_ == ')')
+        readMultiline(newAcc, openParens + newOpen)
 
   while true do
-    print("> ")
-    val line = readLine()
-
-    line match
-      case "exit" => return
-      case _ =>
-        try
-          val expr = Parser.parse(line)
-          val value = eval(expr, globalEnv)
-          println(valueToString(value))
-        catch case e: Exception => println(s"Error: ${e.getMessage}")
+    try
+      val input = readMultiline("", 0)
+      processLine(input)
+    catch
+      case e: Exception => println(s"Error: ${e.getMessage}")
